7006acb258e57e6a7dc410727a7174d2a4cb6749
hierror <hierrodscherer@gmail.com>
2021-05-09 03:34:53 -0300

    styles: Styled the Add Book page

--- src/pages/AddBook/AddBook.tsx
+++ src/pages/AddBook/AddBook.tsx
@@ -1,34 +1,53 @@
 import React from 'react';
+import './AddBook.scss';
 
 const AddBook = () => {
   console.log(process.env);
 
   return (
     <>
-      <div>
+      <div className="title">
         <h1>Add a new book</h1>
       </div>
-      <div>
-        <form>
-          <div>
-            <label htmlFor="name">
+      <div className="form-wrapper">
+        <form className="form">
+          <div className="form__group">
+            <label className="form__label" htmlFor="name">
               Name
-              <input name="name" id="name" type="text" />
             </label>
+            <input
+              className="form__input-text"
+              name="name"
+              id="name"
+              type="text"
+              required
+            />
           </div>
-          <div>
-            <label htmlFor="author">
+          <div className="form__group">
+            <label className="form__label" htmlFor="author">
               Author
-              <input name="author" id="author" type="text" />
             </label>
+            <input
+              className="form__input-text"
+              name="author"
+              id="author"
+              type="text"
+              required
+            />
           </div>
-          <div>
-            <label htmlFor="description">
-              Author
-              <textarea name="description" id="description" />
+          <div className="form__group">
+            <label className="form__label" htmlFor="description">
+              Description
             </label>
+            <textarea
+              className="form__text-area"
+              name="description"
+              id="description"
+              rows={6}
+              required
+            />
           </div>
-          <input type="submit" value="Add new book" />
+          <input className="form__submit" type="submit" value="Add new book" />
         </form>
       </div>
     </>
